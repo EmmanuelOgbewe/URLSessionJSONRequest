6327767f1a8a1c7255d73accafdf382d5755d267
Emmanuel Ogbewe <emmanuelogbewe@Emmanuels-MacBook-Pro.local>
2018-04-06 15:15:41 -0400

    Initial Commit

---
+++ URLSessionJSONRequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  URLSessionJSONRequest
+//
+//  Created by Emmanuel  Ogbewe on 4/6/18.
+//  Copyright © 2018 Emmanuel Ogbewe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
